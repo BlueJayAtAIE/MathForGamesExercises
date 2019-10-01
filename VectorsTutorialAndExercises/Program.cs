ae6250d7318222fe91abba917c8a168c49c90eb9
Julia Pickett-Garcia <s199064@students.aie.edu>
2019-09-30 17:48:13 -0700

    Added stuff from Dot and Cross Product exercises.

--- VectorsTutorialAndExercises/Program.cs
+++ VectorsTutorialAndExercises/Program.cs
@@ -74,6 +74,40 @@ namespace VectorsTutorialAndExercises
             {
                 return (this / Magnitude());
             }
+            
+            /// <summary>
+            /// Returns a perpendicular Vector2 facing Right relitive to the original Vector2.
+            /// </summary>
+            public Vector2 GetPerpendicular()
+            {
+                return new Vector2(-y, x);
+            }
+
+            /// <summary>
+            /// Returns a perpendicular Vector2 facing Left relitive to the original Vector2.
+            /// </summary>
+            /// <param name="isFacingLeft">If true, returns a left facing perpendicular Vector2.</param>
+            public Vector2 GetPerpendicular(bool isFacingLeft)
+            {
+                if (isFacingLeft)
+                {
+                    return new Vector2(y, -x);
+                }
+                return GetPerpendicular();
+            }
+
+            /// <summary>
+            /// Finds the angle in radians between the Vector2 and specified other Vector2.
+            /// </summary>
+            /// <param name="compareTo">Other Vector2 to compare to for angle calculation.</param>
+            public float AngleBetween(Vector2 compareTo)
+            {
+                // Normalize both Vector3s
+                Vector2 a = GetNormalized();
+                Vector2 b = compareTo.GetNormalized();
+
+                return (float)Math.Acos(a.DotProduct(b));
+            }
 
             /// <summary>
             /// DEBUG TOOL: prints the values of the Vector2 to the console.
@@ -121,6 +155,12 @@ namespace VectorsTutorialAndExercises
             public static Vector2 operator /(float lhs, Vector2 rhs)
             {
                 return new Vector2(lhs / rhs.x, lhs / rhs.y);
+            }
+
+            // Dot Product
+            public float DotProduct(Vector2 rhs)
+            {
+                return x * rhs.x + y * rhs.y;
             }
         }
 
@@ -196,6 +236,19 @@ namespace VectorsTutorialAndExercises
             }
 
             /// <summary>
+            /// Finds the angle in radians between the Vector3 and specified other Vector3.
+            /// </summary>
+            /// <param name="compareTo">Other Vector3 to compare to for angle calculation.</param>
+            public float AngleBetween(Vector3 compareTo)
+            {
+                // Normalize both Vector3s
+                Vector3 a = GetNormalized();
+                Vector3 b = compareTo.GetNormalized();
+
+                return (float)Math.Acos(a.DotProduct(b));
+            }
+
+            /// <summary>
             /// DEBUG TOOL: prints the values of the Vector3 to the console.
             /// </summary>
             public void PrintValues(string name)
@@ -242,48 +295,85 @@ namespace VectorsTutorialAndExercises
             {
                 return new Vector3(lhs / rhs.x, lhs / rhs.y, lhs / rhs.z);
             }
+
+            // Dot Product
+            public float DotProduct(Vector3 rhs)
+            {
+                return x * rhs.x + y * rhs.y + z * rhs.z;
+            }
+
+            // Cross Product
+            public Vector3 CrossProduct(Vector3 rhs)
+            {
+                return new Vector3(
+               y * rhs.z - z * rhs.y,
+               z * rhs.x - x * rhs.z,
+               x * rhs.y - y * rhs.x);
+            }
         }
 
 
         static void Main(string[] args)
         {
             float userInput;
-            Vector3 test = new Vector3(10,10,10);
-            Vector3 testAddSub = new Vector3(2, 2, 2);
-            float testMulDiv = 2;
 
-            Vector3 userMade = new Vector3();
-            Console.WriteLine("Build me a Vector3. Enter an X value: ");
-            float.TryParse(Console.ReadLine(), out userInput);
-            userMade.x = userInput;
-            Console.WriteLine("Enter a Y value: ");
-            float.TryParse(Console.ReadLine(), out userInput);
-            userMade.y = userInput;
-            Console.WriteLine("Enter a Z value: ");
-            float.TryParse(Console.ReadLine(), out userInput);
-            userMade.z = userInput;
+            // Dot Product Tests ---------------------------------------------------
+            Vector2 TwoAOne = new Vector2(1, 0);
+            Vector2 TwoATwo = new Vector2(0, 1);
+            Console.WriteLine(TwoAOne.DotProduct(TwoATwo));
 
-            userMade.PrintMagnitude("User Made");
+            Vector2 TwoBOne = new Vector2(1, 1);
+            Vector2 TwoBTwo = new Vector2(-1, -1);
+            Console.WriteLine(TwoBOne.DotProduct(TwoBTwo));
 
-            Vector2 A31 = new Vector2(-2, 5.5f);
-            Vector2 A32 = new Vector2(9, -22);
-            Console.WriteLine(A31.Distance(A32));
+            Vector3 TwoCOne = new Vector3(2, 3, 1);
+            Vector3 TwoCTwo = new Vector3(-3, 1, 2);
+            Console.WriteLine(TwoCOne.DotProduct(TwoCTwo));
 
-            Vector3 B31 = new Vector3(0, 1 , 2);
-            Vector3 B32 = new Vector3(9, -2, 7);
-            Console.WriteLine(B31.Distance(B32));
+            // Angle Between Tests -------------------------------------------------
+            Vector2 ThreeAOne = new Vector2(1, 3);
+            Vector2 ThreeATwo = new Vector2(0.5f, -0.25f);
+            Console.WriteLine(ThreeAOne.AngleBetween(ThreeATwo));
+
+            Vector3 ThreeBOne = new Vector3(-0.5f, 0, 2);
+            Vector3 ThreeBTwo = new Vector3(-1, 0, -1);
+            Console.WriteLine(ThreeBOne.AngleBetween(ThreeBTwo));
+
+            // Magnitude Test ------------------------------------------------------
+            //Vector3 userMade = new Vector3();
+            //Console.WriteLine("Build me a Vector3. Enter an X value: ");
+            //float.TryParse(Console.ReadLine(), out userInput);
+            //userMade.x = userInput;
+            //Console.WriteLine("Enter a Y value: ");
+            //float.TryParse(Console.ReadLine(), out userInput);
+            //userMade.y = userInput;
+            //Console.WriteLine("Enter a Z value: ");
+            //float.TryParse(Console.ReadLine(), out userInput);
+            //userMade.z = userInput;
+
+            //userMade.PrintMagnitude("User Made");
+
+            // Distance Tests ------------------------------------------------------
+            //Vector2 A31 = new Vector2(-2, 5.5f);
+            //Vector2 A32 = new Vector2(9, -22);
+            //Console.WriteLine(A31.Distance(A32));
+
+            //Vector3 B31 = new Vector3(0, 1 , 2);
+            //Vector3 B32 = new Vector3(9, -2, 7);
+            //Console.WriteLine(B31.Distance(B32));
 
             //test.PrintValues("Test");
 
+            // Opperation Tests ----------------------------------------------------
+            //Vector3 test = new Vector3(10,10,10);
+            //Vector3 testAddSub = new Vector3(2, 2, 2);
+            //float testMulDiv = 2;
             //test += testAddSub;
             //test.PrintValues("Test");
-
             //test -= testAddSub;
             //test.PrintValues("Test");
-
             //test *= testMulDiv;
             //test.PrintValues("Test");
-
             //test /= testMulDiv;
             //test.PrintValues("Test");
 
