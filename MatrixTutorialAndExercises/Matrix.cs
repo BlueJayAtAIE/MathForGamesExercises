353b38225735adf32bd5c4403ff3f5c0c064316f
Julia Pickett-Garcia <s199064@students.aie.edu>
2019-10-02 12:49:32 -0700

    M a t r i x  transformations. But only the basic ones.

--- MatrixTutorialAndExercises/Matrix.cs
+++ MatrixTutorialAndExercises/Matrix.cs
@@ -16,14 +16,23 @@ namespace MatrixTutorialAndExercises
 
             public float m1, m2, m3, m4;
 
-            public static Matrix2 identity = new Matrix2(1, 0, 0, 1);            /// <summary>
+            public static Matrix2 identity = new Matrix2(1, 0, 0, 1);
+
+            /// <summary>
             /// Creates a Matrix2 to the specified floats.
             /// </summary>
             public Matrix2(float M1, float M2, float M3, float M4)
             {
                 m1 = M1; m2 = M3;
-                m3 = M3; m4 = M4;
-            }
+                m3 = M2; m4 = M4;
+            }
+
+            void Set(Matrix2 m)
+            {
+                m1 = m.m1; m3 = m.m3;
+                m2 = m.m2; m4 = m.m4;
+            }
+
             /// <summary>
             /// Returns the transpotition of the Matrix2.
             /// </summary>
@@ -32,6 +41,66 @@ namespace MatrixTutorialAndExercises
                 return new Matrix2(
                     m1, m2,
                     m3, m4);
+            }
+
+            // Scaling --------------------------------------
+            public void SetScaled(float x, float y)
+            {
+                m1 = x; m3 = 0;
+                m2 = 0; m4 = y;
+            }
+
+            public void SetScaled(Vector2 v)
+            {
+                m1 = v.x; m3 = 0;
+                m2 = 0; m4 = v.y;
+            }
+
+            public void Scale(float x, float y)
+            {
+                Matrix2 m = new Matrix2();
+                m.SetScaled(x, y);
+
+                Set(this * m);
+            }
+
+            public void Scale(Vector2 v)
+            {
+                Matrix2 m = new Matrix2();
+                m.SetScaled(v);
+
+                Set(this * m);
+            }
+
+            // Rotating ----------------------------------------------
+            // X
+            public void SetRotateX(double radians)
+            {
+                Matrix2 m = new Matrix2(
+                    1, (float)-Math.Sin(radians),
+                    0, (float)Math.Cos(radians));
+
+                Set(m);
+            }            public void RotateX(double radians)
+            {
+                Matrix2 m = new Matrix2();
+                m.SetRotateX(radians);
+                Set(this * m);
+            }
+
+            // Y
+            public void SetRotateY(double radians)
+            {
+                Matrix2 m = new Matrix2(
+                    (float)Math.Cos(radians), 0,
+                    (float)Math.Sin(radians), 1);
+
+                Set(m);
+            }            public void RotateY(double radians)
+            {
+                Matrix2 m = new Matrix2();
+                m.SetRotateY(radians);
+                Set(this * m);
             }
 
             /// <summary>
@@ -65,7 +134,9 @@ namespace MatrixTutorialAndExercises
 
             public float m1, m2, m3, m4, m5, m6, m7, m8, m9;
 
-            public static Matrix3 identity = new Matrix3(1, 0, 0, 0, 1, 0, 0, 0, 1);            /// <summary>
+            public static Matrix3 identity = new Matrix3(1, 0, 0, 0, 1, 0, 0, 0, 1);
+
+            /// <summary>
             /// Creates a Matrix3 to the specified floats.
             /// </summary>
             public Matrix3(float M1, float M2, float M3, float M4, float M5, float M6, float M7, float M8, float M9)
@@ -82,7 +153,18 @@ namespace MatrixTutorialAndExercises
                 m1 = M1; m4 = M2; m7 = M3;
                 m2 = M4; m5 = M5; m8 = M6;
                 m3 = M7; m6 = M8; m9 = M9;
-            }
+            }
+
+            /// <summary>
+            /// Sets the Matrix3 to the supplied Matrix3.
+            /// </summary>
+            void Set(Matrix3 m)
+            {
+                m1 = m.m1; m4 = m.m4; m7 = m.m7;
+                m2 = m.m2; m5 = m.m5; m8 = m.m8;
+                m3 = m.m3; m6 = m.m6; m9 = m.m9;
+            }
+
             /// <summary>
             /// Returns the transpotition of the Matrix3.
             /// </summary>
@@ -94,6 +176,98 @@ namespace MatrixTutorialAndExercises
                     m7, m8, m9);
             }
 
+            // Scaling --------------------------------------
+            public void SetScaled(float x, float y, float z)
+            {
+                m1 = x; m4 = 0; m7 = 0;
+                m2 = 0; m5 = y; m8 = 0;
+                m3 = 0; m6 = 0; m9 = z;
+            }
+
+            public void SetScaled(Vector3 v)
+            {
+                m1 = v.x; m4 = 0; m7 = 0;
+                m2 = 0; m5 = v.y; m8 = 0;
+                m3 = 0; m6 = 0; m9 = v.z;
+            }
+
+            public void Scale(float x, float y, float z)
+            {
+                Matrix3 m = new Matrix3();
+                m.SetScaled(x, y, z);
+
+                Set(this * m);
+            }
+
+            public void Scale(Vector3 v)
+            {
+                Matrix3 m = new Matrix3();
+                m.SetScaled(v);
+
+                Set(this * m);
+            }
+
+            // Rotating ----------------------------------------------
+            // X
+            public void SetRotateX(double radians)
+            {
+                Matrix3 m = new Matrix3(
+                    1, 0, 0,
+                    0, (float)Math.Cos(radians), (float)Math.Sin(radians),
+                    0, (float)-Math.Sin(radians), (float)Math.Cos(radians));
+
+                Set(m);
+            }            public void RotateX(double radians)
+            {
+                Matrix3 m = new Matrix3();
+                m.SetRotateX(radians);
+                Set(this * m);
+            }
+
+            // Y
+            public void SetRotateY(double radians)
+            {
+                Matrix3 m = new Matrix3(
+                    (float)Math.Cos(radians), 0, (float)-Math.Sin(radians),
+                    0, 1, 0,
+                    (float)Math.Sin(radians), 0, (float)Math.Cos(radians));
+
+                Set(m);
+            }            public void RotateY(double radians)
+            {
+                Matrix3 m = new Matrix3();
+                m.SetRotateY(radians);
+                Set(this * m);
+            }
+
+            // Z
+            public void SetRotateZ(double radians)
+            {
+                Matrix3 m = new Matrix3(
+                    (float)Math.Cos(radians), (float)Math.Sin(radians), 0,
+                    (float)-Math.Sin(radians), (float)Math.Cos(radians), 0,
+                    0, 0, 1);
+
+                Set(m);
+            }            public void RotateZ(double radians)
+            {
+                Matrix3 m = new Matrix3();
+                m.SetRotateZ(radians);
+                Set(this * m);
+            }
+
+            // Euler Angle Based
+            public void SetEuler(float pitch, float yaw, float roll)
+            {
+                Matrix3 x = new Matrix3();
+                Matrix3 y = new Matrix3();
+                Matrix3 z = new Matrix3();
+                x.SetRotateX(pitch);
+                y.SetRotateY(yaw);
+                z.SetRotateZ(roll);
+
+                Set(z * y * x);
+            }
             /// <summary>
             /// DEBUG TOOL: For printing out the numbers in the Matrix3.
             /// </summary>
